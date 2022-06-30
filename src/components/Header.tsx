bf8d6ec9e8ee98d69ed503d8add0e51ebe530e3c
lucassodresa <lucas.sodresa@gmail.com>
2022-07-01 00:46:14 +0100

    Header component created

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import "../styles/header.scss";
+
+interface HeaderProps {
+  title: string;
+}
+
+export function Header(props: HeaderProps) {
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {props.title}</span>
+      </span>
+    </header>
+  );
+}
