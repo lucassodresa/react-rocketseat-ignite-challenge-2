c90298fcd11cc7bfed04899d716d50302de7127b
lucassodresa <lucas.sodresa@gmail.com>
2022-07-01 00:45:35 +0100

    component SideBar created

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,50 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { Button } from "./Button";
+import { api } from "../services/api";
+import "../styles/sidebar.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SideBarProps {
+  setSelectedGenreId: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar(props: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
   // Complete aqui
+
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
